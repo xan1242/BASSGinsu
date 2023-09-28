71e2cd78c074eb378679a85746059718370ec6f4
xan1242 <8014093+xan1242@users.noreply.github.com>
2023-09-29 00:29:06 +0200

    fadeout linear

--- BASSGinsu.hpp
+++ BASSGinsu.hpp
@@ -1134,7 +1134,7 @@ private:
                 float d1 = (freqMax - redlineStart) * s;
                 float d2 = redlineStart + d1;
                 float d3 = std::clamp((inFreq - d2) / (freqMax - d2), 0.0f, 1.0f);
-                float fT = exponentialInterpolation((float)redlineFadeTime, 0.00000001f, d3);
+                float fT = cus_lerp((float)redlineFadeTime, 0.00000001f, d3);
 
                 std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - redlineTime);
                 redlineVol = 1.0f - (float)(duration.count()) / fT;
